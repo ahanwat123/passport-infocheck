883161f6e8d2a9b605dd9d05e0fd4590f1adace5
ahanwat123 <avinashhanwat@zoho.com>
2023-08-02 20:52:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const API_BASE_URL = 'http://localhost:4000'; // Replace with the base URL of your API server
+const API_KEY = 'avinash'; // Replace with your API key
+
+const DocumentUpload = () => {
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [result, setResult] = useState(null);
+  const [error, setError] = useState(null);
+
+  const handleFileChange = (event) => {
+    setSelectedFile(event.target.files[0]);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    if (!selectedFile) {
+      setError('Please select a file.');
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append('document', selectedFile);
+
+    try {
+      const response = await fetch(`${API_BASE_URL}/extract`, {
+        method: 'POST',
+        headers: {
+          Authorization: API_KEY,
+        },
+        body: formData,
+      });
+
+      const data = await response.json();
+      if (response.ok) {
+        setResult(data);
+        setError(null);
+      } else {
+        setError(data.error || 'Something went wrong.');
+        setResult(null);
+      }
+    } catch (error) {
+      setError('Failed to fetch data from the server.');
+      setResult(null);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Upload a Document</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="file" accept=".jpeg, .jpg, .png, .pdf" onChange={handleFileChange} />
+        <button type="submit">Upload and Extract</button>
+      </form>
+      {error && <div style={{ color: 'red' }}>{error}</div>}
+      {result && (
+        <div>
+          <h2>Extracted Data:</h2>
+          <pre>{JSON.stringify(result, null, 2)}</pre>
+        </div>
+      )}
     </div>
   );
-}
+};
 
-export default App;
+export default DocumentUpload;
