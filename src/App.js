404bf27cf2449a3dd185f4cc62998f1f7ce6a4ff
ahanwat123 <avinashhanwat@zoho.com>
2023-10-16 19:56:26 +0530

    jagrata

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-
+import './App.css'
 // Replace the API_BASE_URL and API_KEY with your own values
 const API_BASE_URL = 'https://dataextractengine.onrender.com'; 
 
@@ -8,10 +8,11 @@ const API_KEY = 'avinash';
 
 const DocumentUpload = () => {
   const [selectedFiles, setSelectedFiles] = useState([]);
-  const [result, setResult] = useState(null);
-  const [result1, setResult1] = useState(null);
+  const [result, setResult] = useState();
+  const [result1, setResult1] = useState();
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
+  const [loading1, setLoading1] = useState(false);
   const [file, setFile] = useState(null);
 
   const handleFileChange = (event) => {
@@ -30,36 +31,40 @@ const DocumentUpload = () => {
     setFile(selectedFile);
   };
   const handleConversion = async () => {
+    setLoading1(true)
     if (!file) {
       return;
     }
 
     const formData = new FormData();
-    formData.append('pdf', file);
+    formData.append('file', file);
 
     try {
-      const response = await fetch('https://dataextractengine.onrender.com/convert-pdf', {
+      const response = await fetch(`${API_BASE_URL}/convert-pdf`, {
         method: 'POST',
         body: formData,
       });
 
       const data = await response.json();
         if (response.ok) {
+          setLoading1(false)
           setResult1(data);
           setError(null);
         } else {
+          setLoading1(false)
           setError(data.error || 'Something went wrong.');
           setResult(null);
         }
       
     } catch (error) {
+      setLoading1(false)
       console.error('Error:', error);
     }
   };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-
+setLoading(true)
     if (selectedFiles.length === 0) {
       setError('Please select at least one file.');
       return;
@@ -71,8 +76,8 @@ const DocumentUpload = () => {
     });
 
     try {
-      setLoading(true);
-
+     
+     
       const response = await fetch(`${API_BASE_URL}/extract`, {
         method: 'POST',
         headers: {
@@ -83,6 +88,7 @@ const DocumentUpload = () => {
 
       const data = await response.json();
       if (response.ok) {
+        setLoading(false)
         setResult(data);
         setError(null);
       } else {
@@ -90,17 +96,19 @@ const DocumentUpload = () => {
         setResult(null);
       }
     } catch (error) {
+      setLoading(false);
       setError('Failed to send files to the server.');
       setResult(null);
     } finally {
-      setLoading(false);
+      
     }
   };
 
   return (
     <div>
-      <h1>Upload Documents (Up to 2)</h1>
+      <div className='left-div'>
       <form onSubmit={handleSubmit}>
+      <h1>Upload Documents (Up to 2)</h1>
         <div>
           <label htmlFor="fileInput">Select Files:</label>
           <input
@@ -113,21 +121,21 @@ const DocumentUpload = () => {
         </div>
         <button type="submit">Upload and Extract</button>
       </form>
-      {loading && <div className="spinner"></div>}
       {error && <div style={{ color: 'red' }}>{error}</div>}
-      {result && (
+      {loading==true?<p>Loading</p>:
         <div>
           <h2>Response Data:</h2>
           <pre>{JSON.stringify(result, null, 2)}</pre>
-        </div>
-      )}
+        </div>}  
+        </div>   
+      <div className='right-div'>
+       <h1>Extract driver licence</h1>
+       <input type="file" accept=".pdf, image/*" onChange={handleFileChange2} />
+       <button onClick={handleConversion}>Upload and Extract data</button>
        <div>
-      <h1>PDF to Image Conversion</h1>
-      <input type="file" accept=".pdf" onChange={handleFileChange2} />
-      <button onClick={handleConversion}>Convert PDF to Image</button>
-      <div>
           <h2>Response Data:</h2>
-          <pre>{JSON.stringify(result1, null, 2)}</pre>
+          {loading1==true?<p>Loading</p>:
+          <pre>{JSON.stringify(result1, null, 2)}</pre>}
         </div>
     </div>
     </div>
